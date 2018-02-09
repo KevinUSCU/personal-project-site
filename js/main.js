0a675964435b253a3a3f4b256d9fa5fbbf94d1f9
Kevin Springer <KevinUSCU@outlook.com>
2018-02-09 02:13:02 -0800

    Update site.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Ping Heroku backends to wake them up for live site links
+// Ping Heroku backends to wake them up for live site links prior to click-throughs
 fetch('https://dashdash-frontend.herokuapp.com/', {})
 fetch('https://dashdash-server.herokuapp.com/', {})
 fetch('https://kevinuscu-moodshrooms-server.herokuapp.com/', {})
