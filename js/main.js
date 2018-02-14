89764759b5c495d129ae8ca7ea1c96acabf01690
Kevin Springer <KevinUSCU@outlook.com>
2018-02-14 14:57:39 -0800

    Disable server pinging.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // Ping Heroku backends to wake them up for live site links prior to click-throughs
-fetch('https://dashdash-frontend.herokuapp.com/', {})
-fetch('https://dashdash-server.herokuapp.com/api/templates', {})
-fetch('https://kevinuscu-moodshrooms-server.herokuapp.com/parts', {})
+// fetch('https://dashdash-frontend.herokuapp.com/', {})
+// fetch('https://dashdash-server.herokuapp.com/api/templates', {})
+// fetch('https://kevinuscu-moodshrooms-server.herokuapp.com/parts', {})
